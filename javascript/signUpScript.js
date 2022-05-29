3f9fd8ccecf207fe190e26727b0578250bf21d68
ZIDANE Aiman PCC ISRAEL <zidanaima1@ferrero.com>
2022-05-29 23:12:13 +0300

    update validate fot first and last name , phone number and password

--- javascript/signUpScript.js
+++ javascript/signUpScript.js
@@ -26,7 +26,11 @@ document.getElementById("Submit").addEventListener("click", () => {
   }
 
   // check if all the input true:
-  if (Emailtovalidate(mail.value) == true && checkage(age.value) == true && matchpass(password.value, conpass.value)) {
+  if (firstNameValidation(firstname.value) && lastNameValidation(lastname.value) &&
+    Emailtovalidate(mail.value) && checkage(age.value) &&
+    matchpass(password.value, conpass.value) &&
+    passwordValidation(password.value)
+  ) {
     let obj = {
       first_name: firstname.value,
       last_name: lastname.value,
@@ -40,7 +44,6 @@ document.getElementById("Submit").addEventListener("click", () => {
     };
     arr.push(obj);
     saveLocal();
-    window.location.replace("../signinpage.html");
   }
   // reset inputs value;
   firstname.value = "";
@@ -68,16 +71,35 @@ function setitem() {
   }
 }
 
+// first Name Validation
+function firstNameValidation(fname) {
+  if (fname.length > 2 && fname.length < 9) {
+    return true;
+  }
+  else {
+    alert("enter valid first name");
+  }
+}
+
+// last Name Validation
+function lastNameValidation(fname) {
+  if (fname.length > 2 && fname.length < 9) {
+    return true;
+  }
+  else {
+    alert("enter valid last name");
+  }
+}
+
 //validate email :
 function Emailtovalidate(mail) {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+  if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(mail)) {
     return true;
   } else {
     alert("You have entered an invalid email address!");
 
   }
 }
-
 
 // check if user enter a true age:
 function checkage(age) {
@@ -90,13 +112,59 @@ function checkage(age) {
   }
 }
 
+// password validation
+
+function passwordValidation(pass) {
+  let flag = 0;
+
+  if (!/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(pass)) {
+    flag++;
+    alert("the password must contain special characters !");
+  }
+
+
+
+  if (!/[a-z]/g.test(pass)) {
+    flag++;
+    alert("the password must contain lower case letters !");
+  }
+
+
+  // Validate capital letters
+  ;
+  if (!/[A-Z]/g.test(pass)) {
+    flag++;
+    alert("the password must contain upper case letters !");
+  }
+
+
+  // Validate numbers
+
+  if (! /[0-9]/g.test(pass)) {
+    flag++;
+    alert("the password must contain numbers !");
+  }
+
+  // Validate length
+  if (pass.length < 8) {
+    flag++;
+    alert("the password must be at least 8 characters !");
+  }
+
+  if (flag == 0) {
+    return true;
+  }
+}
+
+
+
 // check match password:
 function matchpass(pass, conpas) {
   if (pass == conpas) {
     return true;
   }
   else {
-    alert("password no match");
+    alert(" password and the confirm not same");
   }
 }
 
@@ -105,3 +173,12 @@ let passwordInput = document.getElementById("passwordInput");
 
 
 
+function checkPassMail() {
+  for (let i = 0; i < arr.length; i++) {
+    if (mailInput.value == arr[i].email && passwordInput.value == arr[i].pass) {
+      console.log("ok");
+    } else {
+      console.log("wrong");
+    }
+  }
+}
